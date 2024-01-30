436ee8e713244f097bd797277ccd4a0b634b9649
Dalton-OKeefe <113914645+Dalton-OKeefe@users.noreply.github.com>
2024-01-30 13:39:52 -0500

    math and input
    testing addition
    testing user input
    *Need to add exception for null values*

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    class Test { 
+    
+    }
     class Program{
         static void Main(string[] args){
-            Console.WriteLine("    /|");
-            Console.WriteLine("   / |");
-            Console.WriteLine("  /  |");
-            Console.WriteLine(" /   |");
-            Console.WriteLine("/____|");
+            Console.WriteLine("Give 2 numbers to add together, pressing enter after each number please");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+                int adding = number1 + number2;
+            Console.WriteLine(adding);
         }
     }
 }
